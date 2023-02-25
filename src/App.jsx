7bebdeedadee4b8a897bfca603d8c677930d8f7d
bombamishappy <pattharaporn.t19@gmail.com>
2023-02-25 22:39:35 +0700

    add more function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,60 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSquareXmark } from '@fortawesome/free-solid-svg-icons';
+import { faL, faSquareXmark } from '@fortawesome/free-solid-svg-icons';
 import './App.css'
 
 function App() {
   const [result, setResult] = useState("0");
   const [cals, setCals] = useState("0");
+  const [isNew, setIsNew] = useState(false);
+
+  function convertResultToListNoOper() {
+    let arr = ["/", "+", "*", "-"]
+    let res = result
+
+    for (let v of arr) {
+      res = res.replaceAll(v, ",")
+    }
+    res = res.split(',')
+    return res
+  }
+
+  function convertResultToListWithOper() {
+    return result.match(/[+\-*/]|\d+\.\d+|\d+/g)
+  }
+
+  function handleClickOperator(object) {
+    let oper = ["/", "+", "*", "-"]
+    let isOper = oper.includes(result.slice(-1))
+
+    if (result === "0") {
+      return
+    }
+
+    if (isOper) {
+      return
+    }
+
+    if (isNew) {
+      setResult(() => `${cals.toString()}${object.target.name}`)
+      setIsNew(() => false)
+      return
+    }
+
+    setResult((oldResult) => oldResult.concat(object.target.name))
+  }
 
 
   function handleClick(object) {
     if (result === "0") {
       setResult(() => "")
     }
+    if (isNew) {
+      setResult(() => "")
+      setIsNew(() => false)
+    }
+
     setResult((oldResult) => oldResult.concat(object.target.name))
   }
 
@@ -27,7 +69,7 @@ function App() {
 
   function delOne() {
     if (result.length <= 1) {
-      setResult(() => "0")
+      setResult(() => "0");
       return
     }
     setResult((oldResult) => oldResult.slice(0, -1))
@@ -35,8 +77,13 @@ function App() {
 
   /* return บวก ลบ คูญ หาร รวมถึงเลขทศนิยมด้วย */
   function performOperation() {
+    const components = convertResultToListWithOper();
+    console.log(components)
+    if (components.length === 1) {
+      setCals(() => result)
+      return
+    }
 
-    const components = result.match(/[+\-*/]|\d+\.\d+|\d+/g);
     let resultA = parseFloat(components[0]);
     for (let i = 1; i < components.length; i += 2) {
       const operator = components[i];
@@ -51,24 +98,29 @@ function App() {
         resultA /= operand;
       }
       setCals(() => resultA);
+      setIsNew(() => true)
     }
 
   }
 
   function percen() {
-    let arr = ["/", "+", "*", "-"]
-    let res = result
-
-    for (let v of arr) {
-      res = res.replaceAll(v, ",")
-    }
-    res = res.split(',')
-
+    let res = convertResultToListNoOper()
     let lastNum = res[res.length - 1]
     let val = lastNum / 100
     val = val.toString()
 
     setResult((prev) => prev.replace(lastNum, val))
+  }
+
+  function deleteBlock() {
+    let res = convertResultToListWithOper()
+    let str = res.slice(0, -2).join('')
+    if (str === "") {
+      setResult(() => "0");
+      return
+    }
+
+    setResult(() => str);
   }
 
 
@@ -98,16 +150,16 @@ function App() {
             <button name="7" onClick={handleClick} class="number">7</button>
             <button name="8" onClick={handleClick} class="number">8</button>
             <button name="9" onClick={handleClick} class="number">9</button>
-            <button onClick={handleClick} class="symbol" id="dot">...</button>
+            <button onClick={deleteBlock} class="symbol" id="dot">...</button>
             <button name="0" onClick={handleClick} class="number">0</button>
-            <button name="." onClick={handleClick} class="number">.</button>
+            <button name="." onClick={handleClickOperator} class="number">.</button>
           </div>
           <div class="right-side">
             <button onClick={delOne} class="del top-symbol" id="right-side" ><FontAwesomeIcon icon={faSquareXmark} /></button>
-            <button name="+" onClick={handleClick} class="symbol" id="right-side">+</button>
-            <button name="-" onClick={handleClick} class="symbol" id="right-side">&ndash;</button>
-            <button name="*" onClick={handleClick} class="symbol" id="right-side">&times;</button>
-            <button name="/" onClick={handleClick} class="symbol" id="right-side">&divide;</button>
+            <button name="+" onClick={handleClickOperator} class="symbol" id="right-side">+</button>
+            <button name="-" onClick={handleClickOperator} class="symbol" id="right-side">&ndash;</button>
+            <button name="*" onClick={handleClickOperator} class="symbol" id="right-side">&times;</button>
+            <button name="/" onClick={handleClickOperator} class="symbol" id="right-side">&divide;</button>
           </div>
         </div>
       </div>
